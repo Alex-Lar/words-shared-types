890944e652c0b6956006daabcf864eca5fcbfd4b
Alexander Larin <doremilarin@gmail.com>
2024-11-28 08:45:33 +0300

    chore: project init

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: '*.ts',
+      options: {
+        printWidth: 100,
+      },
+    },
+  ],
+}
